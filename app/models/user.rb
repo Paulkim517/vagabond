ebc6b2c680b27b8935bef33e30ec6fdb209575f0
Henry Freel <henryfreel@gmail.com>
2015-08-11 10:27:46 -0700

    new paperclip branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,13 @@ class User < ActiveRecord::Base
       message: "not a valid format"
     }
 
+  # attr_accessible :avatar
+  has_attached_file :avatar,
+                     :styles => { :medium => "300x300>", :thumb => "100x100#>" },
+                     :default_url => "/images/:style/missing.png"
+
+  validates_attachment :avatar, :presence => true,
+                        :content_type => { :content_type => ["image/jpeg", "image/jpg", "image/gif", "image/png"] },
+                        :size => { :in => 0..1000.kilobytes }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810214140) do
+ActiveRecord::Schema.define(version: 20150811061240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
