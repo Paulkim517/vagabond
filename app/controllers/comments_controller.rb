95df49550d2063ff9041d8fadb711c00f8a04daa
Sabastian <sswbelser@gmail.com>
2015-08-13 16:45:33 -0700

    Cleaned shit up

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+end
